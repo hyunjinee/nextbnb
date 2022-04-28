adea9f60f90727d848230fa8c2bb7e04a92b2489
hyunjin <leehj0110@kakao.com>
2022-04-29 00:07:48 +0900

    feat: 숙소 예약 완료, nextairbnb 완성

---
+++ pages/room/[id].tsx
@@ -0,0 +1,25 @@
+import { NextPage } from 'next';
+import { getRoomAPI } from '../../lib/api/room';
+import { roomActions } from '../../store/room';
+import RoomDetail from '../../components/room/detail/RoomDetail';
+
+const roomDetail: NextPage = () => {
+  return <RoomDetail />;
+};
+
+// eslint-disable-next-line consistent-return
+roomDetail.getInitialProps = async ({ query, store }) => {
+  const { id } = query;
+
+  try {
+    if (id) {
+      const { data } = await getRoomAPI(Number(id as string));
+      store.dispatch(roomActions.setDetailRoom(data));
+    }
+    return {};
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default roomDetail;
